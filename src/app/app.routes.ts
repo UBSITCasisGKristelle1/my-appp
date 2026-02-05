52352c3bd7d7fa2d8b4d58c471dfce848817ceaa
UBSITCasisGKristelle1 <casiskristelle@gmail.com>
2026-02-05 13:32:37 +0800

    adwdwd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './comps/about/about.component';
+import { LoginComponent } from './comps/login/login.component';
+import { HomeComponent } from './comps/home/home.component';
+import { NaviComponent } from './comps/navi/navi.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { 
+    path: 'login',
+    component: LoginComponent,
+   },
+    {   
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    { 
+        path: 'navi',
+        component: NaviComponent,
+    }
+];
--- src/app/comps/comps.component.ts
+++ src/app/comps/comps.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NaviComponent } from '.comps/navi/navi.component';
 
 @Component({
-  selector: 'app-comps',
-  imports: [],
-  templateUrl: './comps.component.html',
-  styleUrl: './comps.component.css'
+  selector: 'app-root',
+  imports: [NaviComponent,RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class CompsComponent {
-
+title = 'my-appp';
 }
