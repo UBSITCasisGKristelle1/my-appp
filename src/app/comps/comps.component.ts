8fd9208d3fc5f147cc3d29ad548facec62d9da1b
UBSITCasisGKristelle1 <casiskristelle@gmail.com>
2026-02-05 13:11:22 +0800

    ww

---
+++ src/app/comps/comps.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comps',
+  imports: [],
+  templateUrl: './comps.component.html',
+  styleUrl: './comps.component.css'
+})
+export class CompsComponent {
+
+}
